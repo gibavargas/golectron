e43830c996f5da5c15eb120de3aba2fa4672731e
Joao Vitor Guidi <jvguidi@gmail.com>
2026-05-10 16:50:58 -0300

    Disable CEF runtime logging

--- internal/native/cef_linux.go
+++ internal/native/cef_linux.go
@@ -270,7 +270,7 @@ func NewCEFInitializeRequest(appDir string, args []string) (CEFInitializeRequest
 		Settings: CEFSettings{
 			NoSandbox:   true,
 			CachePath:   cachePath,
-			LogSeverity: CEFLogSeverityWarning,
+			LogSeverity: CEFLogSeverityDisable,
 		},
 	}), nil
 }
