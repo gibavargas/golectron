a8f195e8481ea3b105ab8fd8eab1c5bfaa384d2e
Joao Vitor Guidi <jvguidi@gmail.com>
2026-05-10 23:03:33 -0300

    Revert "Skip browser-process CEF switch callback"
    This reverts commit b948506021136c573b4c3aa5f8c69e06e6b2f20d.

--- native/cef_shim.c
+++ native/cef_shim.c
@@ -101,9 +101,7 @@ static void CEF_CALLBACK eg_cef_on_before_command_line_processing(
     const cef_string_t* process_type,
     struct _cef_command_line_t* command_line) {
   (void)self;
-  if (!process_type || process_type->length == 0) {
-    return;
-  }
+  (void)process_type;
   eg_cef_append_ascii_switch(command_line, "disable-background-networking");
   eg_cef_append_ascii_switch(command_line, "disable-breakpad");
   eg_cef_append_ascii_switch(command_line, "disable-component-update");
